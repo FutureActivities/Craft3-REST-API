9974446a43af3c289aa59379e6e06c2cbba4d655
Nzime <people@nzime.com>
2019-05-20 15:55:46 +0000

    Add event to uri check

--- src/controllers/v1/GeneralController.php
+++ src/controllers/v1/GeneralController.php
@@ -3,10 +3,13 @@ namespace futureactivities\rest\controllers\v1;
 
 use Craft;
 use craft\web\Controller;
+use craft\events\ElementEvent;
 use futureactivities\rest\errors\BadRequestException;
 
 class GeneralController extends Controller
 {
+    const EVENT_URI_PROCESS = 'processUri';
+    
     protected $allowAnonymous = true;
     
     public function actionUri($uri)
@@ -25,6 +28,10 @@ class GeneralController extends Controller
         
         if (isset($element->type))
             $result['handle'] = $element->type->handle;
+        
+        $this->trigger(self::EVENT_URI_PROCESS, new ElementEvent([
+            'element' => $element
+        ]));
         
         return $this->asJson($result);
     }
